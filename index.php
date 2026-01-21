edf85dbe0142d3781855487b4dce9182b83ab03c
Baussart Renaud <renaud.baussart@proton.me>
2026-01-21 14:37:49 +0100

    feat: add css to form

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['html_content'])) {
             $htmlContent = $_POST['html_content'];
-            // Initialize Dompdf
             $dompdf = new Dompdf();
             $dompdf->loadHtml($htmlContent);
             $dompdf->setPaper('A4', 'portrait');
             $dompdf->render();
-            // Output the generated PDF to browser
             $dompdf->stream("generated_cv.pdf", ["Attachment" => false]);
         }
     }
